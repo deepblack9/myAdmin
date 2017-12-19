4a1cf15628410bbddfe5a904625e5c8ffe50b8c1
deepblack9 <seed_922@163.com>
2017-12-19 16:38:01 +0800

    modify

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"'
+  BASE_API: '"https://www.easy-mock.com/mock/5a33457cb1a7371615ef9e2d/myadmin"'
 })
