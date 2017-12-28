39fb02844764355f5febd585ae7a37321caf4138
deepblack9 <seed_922@163.com>
2017-12-28 16:49:46 +0800

    modify

--- src/views/quota/Project.vue
+++ src/views/quota/Project.vue
@@ -75,6 +75,11 @@ export default {
       })
     }
   },
+  watch: {
+    filterText(val) {
+      this.$refs.tree2.filter(val)
+    }
+  },
   data() {
     return {
       list: null,
@@ -84,45 +89,6 @@ export default {
       listLoading: true,
 
       filterText: '',
-      data2: [{
-        id: 0,
-        label: '用户档案',
-        children: [{
-          id: 1,
-          label: '一级 1',
-          children: [{
-            id: 4,
-            label: '二级 1-1',
-            children: [{
-              id: 9,
-              label: '三级 1-1-1'
-            }, {
-              id: 10,
-              label: '三级 1-1-2'
-            }]
-          }]
-        }, {
-          id: 2,
-          label: '一级 2',
-          children: [{
-            id: 5,
-            label: '二级 2-1'
-          }, {
-            id: 6,
-            label: '二级 2-2'
-          }]
-        }, {
-          id: 3,
-          label: '一级 3',
-          children: [{
-            id: 7,
-            label: '二级 3-1'
-          }, {
-            id: 8,
-            label: '二级 3-2'
-          }]
-        }]
-      }],
       defaultProps: {
         children: 'children',
         label: 'label'
